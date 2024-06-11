bcba11847de67306187a34cc3c035fa6078386b6
Matiu <mateovranych@hotmail.com>
2024-06-10 23:11:12 -0300

    productos

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDonAldo.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductosController : ControllerBase
+	{
+	}
+}
---
+++ Models/Entities/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiDonAldo.Models.Entities
+{
+	public class Producto
+	{
+
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public string Descripcion { get; set; }
+		public decimal Precio { get; set; }
+		public string ImagenUrl { get; set; } // URL de la imagen
+
+	}
+}
