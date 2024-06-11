424d1747634081d40f378b3fa6a3f1d63a6db0c4
Matiu <mateovranych@hotmail.com>
2024-06-11 18:36:53 -0300

    Controlador, servicio y entidades

---
+++ Migrations/20240611212448_Productos.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiDonAldo.Migrations
+{
+    public partial class Productos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Descripcion = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Precio = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
--- Services/SProductos.cs
+++ Services/SProductos.cs
@@ -1,22 +1,53 @@
-﻿using AutoMapper;
+﻿using ApiDonAldo.Context;
+using ApiDonAldo.Models.DTOs.ProductoDTO;
+using ApiDonAldo.Models.Entities;
+using AutoMapper;
 
 namespace ApiDonAldo.Services
 {
 	public class SProductos
 	{
-        private readonly Mapper mapper;
-        public SProductos(Mapper mapper)
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        
+
+        public SProductos(IMapper mapper, AppDbContext context)
         {
-            this.mapper = mapper;            
+            _context = context;
+            _mapper = mapper;
         }
 
-        public async Task CrearProducto()
+        public async Task<ProductoDTO> CrearProductosAsync(ProductoCreacionDTO productoCreacionDTO)
         {
+            var producto = _mapper.Map<Producto>(productoCreacionDTO);
+            if(productoCreacionDTO.Imagen != null)
+            {
+                var imagenUrl = await GuardarImagenAsync(productoCreacionDTO.Imagen);
+                producto.ImagenUrl = imagenUrl;
+            }
+
+            //_context.Productos.Add(producto);  
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<ProductoDTO>(producto);
 
         }
 
+        public async Task<string> GuardarImagenAsync(IFormFile imagen)
+        {
+            var imagenPath = Path.Combine("wwwroot/images",imagen.FileName);
+
+            using (var stream = new FileStream(imagenPath, FileMode.Create))
+            {
+                await imagen.CopyToAsync(stream);
+            }
 
 
 
-    }
+             return $"/images/{imagen.FileName}";
+		}
+                
+
+
+	}
 }
